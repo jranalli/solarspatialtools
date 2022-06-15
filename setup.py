859bd8cc308fa6a3771f6a6659e916aebff70167
Joe Ranalli <jar339@psu.edu>
2022-06-15 12:05:20 -0400

    Fix a few build and deploy files

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+ver = '0.2.0'
+
+
 setup(
     name='solartoolbox',
-    version='0.2.0',
+    version=ver,
     author="Joe Ranalli",
-    author_email="jar339@psu.edu",
+    author_email="jranalli@psu.edu",
     description="A research toolbox for solar analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/jranalli/solartoolbox",
+    download_url="https://github.com/jranalli/solartoolbox/archive/{}.tar.gz".format(ver),
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
-        'pandas>=1.0',
+        'pandas',
         'tables',  # possibly must be installed separately via conda pytables because of MS dependency
-        'numpy>=1.2',
-        'pyproj>=3.0',
-        'pvlib>=0.9',
+        'numpy',
+        'pyproj',
+        'pvlib',
         'netcdf4',
-        'scipy>=1.6'
+        'scipy'
     ],
     license='BSD (3 Clause)',
     extras_require=dict(tests=['pytest'], demos=['matplotlib']),
