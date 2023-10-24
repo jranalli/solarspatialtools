5d4b76b874bead2ecd2790a69353739f0427a350
Joe Ranalli <jar339@psu.edu>
2023-10-24 16:09:34 -0400

    Add comments to example

--- demos/field_demo.py
+++ demos/field_demo.py
@@ -5,52 +5,91 @@ import matplotlib.pyplot as plt
 from solartoolbox import spatial, field
 
 
+# #############
+# # READ DATA #
+# #############
+
+# This is the name of the default datafile
+# It contains the definition of the plant layout as well as
+# two time periods A) and B) that represent two different CMV periods
 datafile = "data/sample_field.h5"
 
+# Input the CMVs, see cmv_demo.py for examples of how to calculate these
 cmv_a = spatial.pol2rect(9.515372409962954, 0.6196401023982533)
 cmv_b = spatial.pol2rect(8.486245566393151, 2.177365219628146)
 
+# pos_utm is a pandas DataFrame. The index is the combiner ID, and the columns
+# 'E' and 'N' specify the combiner position East and North in a UTM-like
+# geographic projection
 pos_utm = pd.read_hdf(datafile, mode="r", key="latlon")
+
+# df_a contains the individual time series for each combiner. The index is
+# the time (with an arbitrary offset, so it begins at 00:00:00). The columns
+# are keyed by the combiner ID. df_a and df_b each represents a single hour of
+# data with a well defined CMV.
 df_a = pd.read_hdf(datafile, mode="r", key="data_a")
 df_b = pd.read_hdf(datafile, mode="r", key="data_b")
 
-plt.figure(figsize=(8, 6))
-plt.scatter(pos_utm['E'], pos_utm['N'])
 
+# #############################
+# # PERFORM FIELD CALCULATION #
+# #############################
+
+# Create a data frame to hold the predicted data
+# Columns E and N will be the expected original position of the combiner
+# Columns com-E and com-N will be the predicted position coordinates
 df = pd.DataFrame(index=pos_utm.index, columns=['E', 'N', 'com-E', 'com-N'])
 
-for refs in pos_utm.index[5:10]:
+# Calculation is performed on each reference individually. Loop to do so.
+# This may take several minutes, so the indexing uses a subset to save time
+# for demo purposes.
+for ref in pos_utm.index[46:62]:
+
+    # compute_predicted_position performs the entire calculation
+    # it returns the expected aggregate position of the combiner (com)
+    # as well as the prediction based on every individual reference combiner
+    # (pos). pos is only used in interpreting the validity of the data.
     com, pos, _ = field.compute_predicted_position(
-        [df_a, df_b],
-        pos_utm,
-        refs,
-        [cmv_a, cmv_b],
-        mode='preavg',
-        ndownsel=8)
+        [df_a, df_b],  # The dataframes with the two one hour periods
+        pos_utm,  # the dataframe specifying the combiner positions
+        ref,  # the position within pos_utm to calculate about
+        [cmv_a, cmv_b],  # The two individual CMVs for the DFs
+        mode='preavg',  # Mode for downselecting the comparison points
+        ndownsel=8)  # Num points to use for downselecting
 
-    df.loc[refs] = [pos_utm.loc[refs]['E'], pos_utm.loc[refs]['N'], com[0], com[1]]
+    # Add this combiner's calculated values to the output DataFrame
+    df.loc[ref] = [pos_utm.loc[ref]['E'], pos_utm.loc[ref]['N'], com[0], com[1]]
 
-    # Show the quality of the current reference's predicted pos
-    plt.plot([pos_utm['E'][refs], com[0]], [pos_utm['N'][refs], com[1]], 'r-+')
 
-plt.axis('equal')
+# ################
+# # PLOT RESULTS #
+# ################
 
-vscale = 100
-arrow_origin = [-100, 600]
+# Create a figure to show results
+plt.figure(figsize=(6, 6))
+# Plot all default positions
+plt.scatter(pos_utm['E'], pos_utm['N'])
+# For each row, plot a red line showing the predicted position relative to the
+# plans.
+for row in df.iterrows():
+    r = row[1]
+    plt.plot([r['E'], r['com-E']], [r['N'], r['com-N']], 'r-+')
+
+# Plot some arrows to show the CMV
 for cmv, color in zip([cmv_a, cmv_b],['green','blue']):
-    velvec = np.array(spatial.unit(cmv)) * vscale
-    plt.arrow(arrow_origin[0], arrow_origin[1],
-          velvec[0], velvec[1],
-          length_includes_head=True,
-          width=7,
-          head_width=20,
-          color=color)
+    velvec = np.array(spatial.unit(cmv)) * 100
+    plt.arrow(-100, 600, velvec[0], velvec[1],
+              length_includes_head=True, width=7, head_width=20, color=color)
+
+# Some plot config
+plt.axis('equal')
 plt.xlabel('E')
 plt.ylabel('N')
 plt.title(f'All Predicted Positions')
 axes = plt.gca()
 axes.xaxis.set_ticklabels([])
 axes.yaxis.set_ticklabels([])
+plt.tight_layout()
 
 plt.show()
 
