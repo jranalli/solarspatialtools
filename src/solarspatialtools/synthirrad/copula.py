8ee6fef428e909225e9f5892cca66ea81083dc65
Joe Ranalli <jar339@psu.edu>
2026-05-04 15:26:28 -0400

    Progress on copula, implementation of GaussianMixture

--- src/solarspatialtools/synthirrad/copula.py
+++ src/solarspatialtools/synthirrad/copula.py
@@ -1,8 +1,11 @@
 import numpy as np
+from sklearn.mixture import GaussianMixture
+
+# [1] Widen, J. and Munkhammar, J., "Spatio-Temporal Downscaling of Hourly Solar irradiance Data Using Gaussian Copulas," 2019 IEEE 46th Photovoltaic Specialists Conference (PVSC), Chicago, IL, USA, 2019, pp. 3172-3178, doi: 10.1109/PVSC40753.2019.8980922.
 
 def _sigmoid(x, a, c):
     """
-    Generate a sigmoid membership function.
+    Generate a sigmoid membership function. Equation 11 from the paper.
 
     Parameters
     ----------
@@ -20,26 +23,69 @@ def _sigmoid(x, a, c):
     """
     return 1 / (1 + np.exp(-a * (x - c)))
 
-def comp_param_model(x, k, a, c):
-    """
-    Gaussian mixture model component proportions
 
-    Parameters
-    ----------
-    x : np.array
-        The input variable, typically in the range [0, 1].
-    k : float
-        The scalar parameter, controlling the maximum value of the output.
-    a : float
-        The slope parameter, controlling how steep the transition is.
-    c : float
-        The center parameter, controlling the center of the transition.
+def _gaussianMixtureDistribution(csi, meanCSI, params, debug=False):
+    """Build a 1D Gaussian mixture and evaluate its PDF at csi values."""
+    comp_cloud = 1 - params['comp'][0]*_sigmoid(meanCSI, params['comp'][1],params['comp'][2])
+    comp_clear = 1 - comp_cloud
+    mean_clear = params['mean'][0] * comp_cloud * meanCSI + params['mean'][1] * (1 - comp_cloud) * (meanCSI - params['mean'][2])
+    mean_cloud = (meanCSI - comp_clear * mean_clear) / comp_cloud
+    sdev_clear = params['sdevClear'][0] * (1 - _sigmoid(meanCSI, params['sdevClear'][1],params['sdevClear'][2]))
+    sdev_cloud = params['sdevCloud'][0] * _sigmoid(meanCSI, params['sdevCloud'][1], params['sdevCloud'][2])
 
-    Returns
-    -------
+    mu = [mean_cloud, mean_clear]
+    p = [comp_cloud, comp_clear]
 
-    """
-    if not k < 1:
-        raise ValueError('k should be less than 1 to avoid negative numbers')
+    sigma = [sdev_cloud**2, sdev_clear**2]
 
-    return 1 - k * _sigmoid(x, a, c)
+    mu = np.array(mu).reshape(-1, 1)
+    sigma = np.array(sigma).reshape(-1, 1, 1)   # 1D full covariance form
+    p = np.array(p)
+
+    gm = GaussianMixture(n_components=len(p), covariance_type="full")
+    gm.weights_ = p
+    gm.means_ = mu
+    gm.covariances_ = sigma
+    gm.precisions_cholesky_ = 1 / np.sqrt(sigma)
+
+    csi_input = np.asarray(csi)
+    csi_samples = np.atleast_1d(csi_input).reshape(-1, 1)
+    pdf_val = np.exp(gm.score_samples(csi_samples))
+    if csi_input.ndim == 0:
+        pdf_val = float(pdf_val[0])
+
+    if debug:
+        import matplotlib.pyplot as plt
+        plt.plot(csi,pdf_val)
+        plt.show()
+
+    return gm, pdf_val
+
+if __name__ == "__main__":
+    param = {
+        'comp': [
+            0.8051,
+            7.3605,
+            0.7092
+        ],
+        'mean': [
+            2.2928,
+            1.0801,
+            0.4532
+        ],
+        'sdevClear': [
+            0.3512,
+            4.8414,
+            0.6442
+        ],
+        'sdevCloud': [
+            0.1997,
+            5.0919,
+            0.3863
+        ]
+    }
+
+    csi = np.arange(-2, 2, 0.01)
+
+    _gaussianMixtureDistribution(csi, 0.52, param, True)
+
--- tests/synthirrad/test_copula.py
+++ tests/synthirrad/test_copula.py
@@ -8,6 +8,7 @@ from solarspatialtools.synthirrad import copula
 import pandas as pd
 
 class TestBasics:
+
     def test_sigmoid_centerpoint(self):
         assert copula._sigmoid(0.5,1,0.5) == approx(0.5)
         assert copula._sigmoid(1, 10, 1) == approx(0.5)
@@ -30,3 +31,32 @@ class TestBasics:
         assert copula._sigmoid(0.25, 2, 0.5) == approx(0.3775, abs=0.001)
         assert copula._sigmoid(0.75, 10, 0.3) == approx(0.989, abs=0.001)
         assert copula._sigmoid(20, 0.5, 30) == approx(0.0067, abs=0.0001)
+
+
+class TestGMM:
+    _params = {
+        'comp': [0.8051, 7.3605, 0.7092],
+        'mean': [2.2928, 1.0801, 0.4532],
+        'sdevClear': [0.3512, 4.8414, 0.6442],
+        'sdevCloud': [0.1997, 5.0919, 0.3863],
+    }
+
+    _expect = {
+        'in': np.array([-1,-0.5,0,0.5,0.8,0.1]),
+        'out': np.array([0.0000, 0.0000, 0.0145, 2.1843, 0.2287, 0.1231])
+    }
+
+    def test_gaussian_mixture_distribution_scalar_csi(self):
+        gm, _ = copula._gaussianMixtureDistribution(0.1, 0.52, self._params)
+
+        assert gm.means_.shape == (2, 1)
+
+    def test_gaussian_mixture_distribution_vector_csi(self):
+        csi = np.linspace(-2, 2, 21)
+        gm, _ = copula._gaussianMixtureDistribution(csi, 0.52, self._params)
+
+        assert gm.means_.shape == (2, 1)
+
+    def test_gaussian_mixture_distribution_matlabvals(self):
+        gm, pdf_val = copula._gaussianMixtureDistribution(self._expect['in'], 0.52, self._params)
+        assert pdf_val == approx(self._expect['out'], abs=0.001)
