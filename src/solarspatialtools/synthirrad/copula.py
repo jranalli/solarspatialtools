329d29a153a3e894a6f6ed82f29ac4f0d07526b2
Joe Ranalli <jar339@psu.edu>
2026-06-04 15:36:04 -0400

    Data now similar to matlab, needs validation. Was caused by an axis issue in the reshaping.

--- src/solarspatialtools/synthirrad/copula.py
+++ src/solarspatialtools/synthirrad/copula.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.mixture import GaussianMixture
 
 from solarspatialtools import spatial
-from scipy.stats import norm
+from scipy.stats import norm, multivariate_normal
 
 # [1] Widen, J. and Munkhammar, J., "Spatio-Temporal Downscaling of Hourly Solar irradiance Data Using Gaussian Copulas," 2019 IEEE 46th Photovoltaic Specialists Conference (PVSC), Chicago, IL, USA, 2019, pp. 3172-3178, doi: 10.1109/PVSC40753.2019.8980922.
 
@@ -28,20 +28,11 @@ def _sigmoid(x, a, c):
     return 1 / (1 + np.exp(-a * (x - c)))
 
 
-def _gaussianMixtureDistribution(csi, meanCSI, params, debug=False):
-    """Build a 1D Gaussian mixture and evaluate its PDF at csi values."""
-    comp_cloud = 1 - params['comp'][0]*_sigmoid(meanCSI, params['comp'][1],params['comp'][2])
-    comp_clear = 1 - comp_cloud
-    mean_clear = params['mean'][0] * comp_cloud * meanCSI + params['mean'][1] * (1 - comp_cloud) * (meanCSI - params['mean'][2])
-    mean_cloud = (meanCSI - comp_clear * mean_clear) / comp_cloud
-    sdev_clear = params['sdevClear'][0] * (1 - _sigmoid(meanCSI, params['sdevClear'][1],params['sdevClear'][2]))
-    sdev_cloud = params['sdevCloud'][0] * _sigmoid(meanCSI, params['sdevCloud'][1], params['sdevCloud'][2])
-
-    mu = [mean_cloud, mean_clear]
-    p = [comp_cloud, comp_clear]
-
-    sigma = [sdev_cloud**2, sdev_clear**2]
-
+def _gmdistribution(x, mu, sigma, p, debug=False):
+    """
+    Wrap sklearn's GaussianMixture to behave more like the matlab gmdistribution function.
+    Desired outputs for subsequent code are the pdf and cdf. CDF is scaled from 0 - 1.
+    """
     mu = np.array(mu).reshape(-1, 1)
     sigma = np.array(sigma).reshape(-1, 1, 1)   # 1D full covariance form
     p = np.array(p)
@@ -52,56 +43,24 @@ def _gaussianMixtureDistribution(csi, meanCSI, params, debug=False):
     gm.covariances_ = sigma
     gm.precisions_cholesky_ = 1 / np.sqrt(sigma)
 
-    csi_input = np.asarray(csi)
-    csi_samples = np.atleast_1d(csi_input).reshape(-1, 1)
-    pdf_val = np.exp(gm.score_samples(csi_samples))
-    if csi_input.ndim == 0:
+    x_input = np.asarray(x)
+    x_samples = np.atleast_1d(x_input).reshape(-1, 1)
+    pdf_val = np.exp(gm.score_samples(x_samples))
+    cdf_val = np.cumsum(pdf_val) / np.max(np.cumsum(pdf_val))
+    if x_input.ndim == 0:
         pdf_val = float(pdf_val[0])
+        cdf_val = float(cdf_val[0])
 
     if debug:
         import matplotlib.pyplot as plt
-        plt.plot(csi,pdf_val)
+        plt.plot(x, pdf_val)
+        plt.figure()
+        plt.plot(x, cdf_val)
         plt.show()
 
-    return gm, pdf_val
+    return gm, pdf_val, cdf_val
 
-def _exponential_decay_parameter(mean_csi, p):
-    k = p * mean_csi * (1-mean_csi)
-    if k < 10**-5:
-        k = 10**-5
-    return k
-
-def _space_time_copula(N, sites, times, csi, cdf, funchand, p, cs, cd):
-    xref, yref = spatial.latlon2lcs(sites[0], sites[1], sites[0][0], sites[1][0])
-    xref, yref = spatial.lla2flat(sites[0], sites[1], sites[0][0], sites[1][0])
-
-    t0 = times[0]
-    dur = (times-t0).total_seconds().values
-
-    # Build a time-dependent drift term and broadcast against site coordinates.
-    x_drift = np.asarray(cs) * dur * np.sin(np.asarray(cd))
-    y_drift = np.asarray(cs) * dur * np.cos(np.asarray(cd))
-
-    X = np.asarray(xref)[:, None] - np.asarray(x_drift)[None, :]
-    Y = np.asarray(yref)[:, None] - np.asarray(y_drift)[None, :]
-    X = X.T
-    Y = Y.T
-
-    x = np.asarray(X).reshape(-1, order='F')
-    y = np.asarray(Y).reshape(-1, order='F')
-    dx = x[:, None] - x[None, :]
-    dy = y[:, None] - y[None, :]
-    D = np.sqrt(dx**2 + dy**2)
-
-    C = funchand(p, D)
-
-    U = copularnd_gaussian(C, N)
-
-    CSI = inverse_sample(csi, cdf, U)
-    CSI = CSI.reshape(N, times.shape[0], sites[0].shape[0])
-    return CSI
-
-def inverse_sample(x, cdf, r):
+def _inverse_sample(x, cdf, r):
     x = np.asarray(x, dtype=float).reshape(-1)
     cdf = np.asarray(cdf, dtype=float).reshape(-1)
 
@@ -130,6 +89,97 @@ def inverse_sample(x, cdf, r):
     return s.reshape(r_arr.shape)
 
 
+def _solar_gmm(csi, meanCSI, params, debug=False):
+    """Build a 1D Gaussian mixture and evaluate its PDF at csi values."""
+    comp_cloud = 1 - params['comp'][0]*_sigmoid(meanCSI, params['comp'][1],params['comp'][2])
+    comp_clear = 1 - comp_cloud
+    mean_clear = params['mean'][0] * comp_cloud * meanCSI + params['mean'][1] * (1 - comp_cloud) * (meanCSI - params['mean'][2])
+    mean_cloud = (meanCSI - comp_clear * mean_clear) / comp_cloud
+    sdev_clear = params['sdevClear'][0] * (1 - _sigmoid(meanCSI, params['sdevClear'][1],params['sdevClear'][2]))
+    sdev_cloud = params['sdevCloud'][0] * _sigmoid(meanCSI, params['sdevCloud'][1], params['sdevCloud'][2])
+
+    mu = [mean_cloud, mean_clear]
+    p = [comp_cloud, comp_clear]
+    sigma = [sdev_cloud**2, sdev_clear**2]
+
+    gm, pdf_val, cdf_val = _gmdistribution(csi, mu, sigma, p, debug)
+
+    return gm, pdf_val, cdf_val
+
+def _exponential_decay_parameter(K, p):
+    k = p * K * (1-K)
+    if k < 10**-5:
+        k = 10**-5
+    return k
+
+
+def _get_spacetime_distances(sites, times, cs, cd):
+    # xref, yref = spatial.latlon2lcs(sites[0], sites[1], sites[0][0], sites[1][0])
+    xref, yref = spatial.lla2flat(sites[0], sites[1], sites[0][0], sites[1][0], 'matlab')
+
+    t0 = times[0]
+    dur = (times-t0).total_seconds().values
+
+    # Build a time-dependent drift term and broadcast against site coordinates.
+    x_drift = np.asarray(cs) * dur * np.sin(np.asarray(cd))
+    y_drift = np.asarray(cs) * dur * np.cos(np.asarray(cd))
+
+    X = np.asarray(xref)[:, None] - np.asarray(x_drift)[None, :]
+    Y = np.asarray(yref)[:, None] - np.asarray(y_drift)[None, :]
+    X = X.T
+    Y = Y.T
+
+    x = np.asarray(X).reshape(-1, order='F')
+    y = np.asarray(Y).reshape(-1, order='F')
+    dx = x[:, None] - x[None, :]
+    dy = y[:, None] - y[None, :]
+    D = np.sqrt(dx**2 + dy**2)
+
+    return D
+
+def _space_time_copula(N, sites, times, csi, cdf, funchand, p, cs, cd):
+    D = _get_spacetime_distances(sites, times, cs, cd)
+
+    C = funchand(p, D)
+
+    U = copularnd_gaussian(C, N)
+
+    CSI = _inverse_sample(csi, cdf, U)
+
+    CSI = CSI.reshape(N, sites[0].shape[0], times.shape[0]).transpose([0,2,1])
+
+    return CSI
+
+
+def scatter_hist(x, y):
+    import matplotlib.pyplot as plt
+
+    fig, axs = plt.subplot_mosaic([['histx', '.'],
+                                   ['scatter', 'histy']],
+                                  figsize=(6, 6),
+                                  width_ratios=(4, 1), height_ratios=(1, 4),
+                                  layout='constrained')
+    ax = axs['scatter']
+    ax_histx = axs['histx']
+    ax_histy = axs['histy']
+
+    # no labels
+    ax_histx.tick_params(axis="x", labelbottom=False)
+    ax_histy.tick_params(axis="y", labelleft=False)
+
+    # the scatter plot:
+    ax.scatter(x, y)
+
+    # now determine nice limits by hand:
+    binwidth = 0.25
+    xymax = max(np.max(np.abs(x)), np.max(np.abs(y)))
+    lim = (int(xymax/binwidth) + 1) * binwidth
+
+    bins = np.arange(-lim, lim + binwidth, binwidth)
+    ax_histx.hist(x, bins=bins)
+    ax_histy.hist(y, bins=bins, orientation='horizontal')
+    plt.show()
+
 def copularnd_gaussian(C, N, random_state=None):
 
     rng = np.random.default_rng(random_state)
@@ -138,7 +188,46 @@ def copularnd_gaussian(C, N, random_state=None):
     U = norm.cdf(z)
     return U
 
+def __basic_testing():
+    # Generate the shape of a gaussian mixture model
+    x0 = np.arange(-2, 2, 0.01)
+    mu0 = np.array([0.25, 0.75])
+    sigma0 = np.array([0.01,0.01])
+    p0 = np.array([1,4])
+
+    # Generate the mixture distribution pdf and cdf
+    _, pdf, cdf = _gmdistribution(x0, mu0, sigma0, p0)
+
+    # Test Inverse Sampling on the CDF
+    yi = [0.6, 0.9]
+    xi = _inverse_sample(x0, cdf, yi)
+
+    print(xi)
+
+    # Plot the output
+    import matplotlib.pyplot as plt
+    plt.plot(x0, pdf)
+    plt.figure()
+    plt.plot(x0, cdf)
+    plt.plot(xi, yi, 'x')
+    plt.show()
+
+    ### CONFIDENT IN THIS OUTPUT, IT WORKS
+
+def __lla2flat_testing():
+    lat = np.array([21.31236, 21.31303, 21.32357])
+    lon = np.array([-158.08463, -158.08505, -158.08424])
+
+    print(spatial.lla2flat(lat, lon, lat[0], lon[0], "matlab"))
+    print(spatial.lla2flat(lat, lon, lat[0], lon[0], "mercator"))
+
+
 if __name__ == "__main__":
+    # __basic_testing()  # Validated - confident in what this is doing
+    # __lla2flat_testing()  # Mercator and lla2flat algorithm are accurate to centimeters for the latlon given
+
+
+
     param = {
         'comp': [
             0.8051,
@@ -187,13 +276,7 @@ if __name__ == "__main__":
         csi = np.arange(-2, 2, 0.01)
         mean_csi = hcsi[i]
 
-        gm, pdf = _gaussianMixtureDistribution(csi, mean_csi, param, debug=False)
-        cdf = np.cumsum(pdf) * (csi[1] - csi[0])
-
-        # Skip matplotlib for now
-        # import matplotlib.pyplot as plt
-        # plt.plot(csi, cdf)
-        # plt.show()
+        gm, pdf, cdf = _solar_gmm(csi, mean_csi, param, debug=False)
 
         fun = lambda p, d: np.exp(-p * d)
         p = _exponential_decay_parameter(mean_csi, param['corr_quadr'])
--- tests/synthirrad/test_copula.py
+++ tests/synthirrad/test_copula.py
@@ -1,16 +1,13 @@
-import pytest
 from pytest import approx
-from numpy import testing as npt
 import numpy as np
 
 from solarspatialtools.synthirrad import copula
 
-import pandas as pd
 
 class TestBasics:
 
     def test_sigmoid_centerpoint(self):
-        assert copula._sigmoid(0.5,1,0.5) == approx(0.5)
+        assert copula._sigmoid(0.5, 1, 0.5) == approx(0.5)
         assert copula._sigmoid(1, 10, 1) == approx(0.5)
 
     def test_sigmoid_asymptotes(self):
@@ -32,8 +29,35 @@ class TestBasics:
         assert copula._sigmoid(0.75, 10, 0.3) == approx(0.989, abs=0.001)
         assert copula._sigmoid(20, 0.5, 30) == approx(0.0067, abs=0.0001)
 
+    def test_exponential_decay_parameter(self):
+        assert copula._exponential_decay_parameter(0.5, 10) == approx(2.5)
+        assert copula._exponential_decay_parameter(0.5, 1) == approx(0.25)
+        assert copula._exponential_decay_parameter(0.5, 0) == approx(10**-5)
+        assert copula._exponential_decay_parameter(0.01, 10) == approx(0.0990, abs=0.0001)
+        assert copula._exponential_decay_parameter(0.99, 10) == approx(0.0990, abs=0.0001)
+        assert copula._exponential_decay_parameter(0.0001, 10) == approx(0.0009999, abs=0.000001)
+        assert copula._exponential_decay_parameter(0.9999, 10) == approx(0.0009999, abs=0.000001)
 
-class TestGMM:
+
+class TestGMDistribution:
+    _params = {
+        'mean': [0.25, 0.75],
+        'sigma': [0.025, 0.025],
+        'p': [8, 4]
+    }
+    _expect = {'x': [0.02, 0.49, 0.92],
+               'y_cdf': [0.0516, 0.6441, 0.9553]}
+
+    def test_gmdistribution_matlab(self):
+        x = np.arange(-2, 2, 0.01)
+        _, _, y = copula._gmdistribution(x, self._params['mean'], self._params['sigma'], self._params['p'])
+        # extract the y values that correspond to the 3 values in _expect['x']
+        y_vals = [y[np.argmin(np.abs(x - target))] for target in self._expect['x']]
+
+        assert y_vals == approx(self._expect['y_cdf'], abs=0.001)
+
+
+class TestSolarGMM:
     _params = {
         'comp': [0.8051, 7.3605, 0.7092],
         'mean': [2.2928, 1.0801, 0.4532],
@@ -42,23 +66,23 @@ class TestGMM:
     }
 
     _expect = {
-        'in': np.array([-1,-0.5,0,0.5,0.8,0.1]),
+        'in': np.array([-1, -0.5, 0, 0.5, 0.8, 0.1]),
         'out': np.array([0.0000, 0.0000, 0.0145, 2.1843, 0.2287, 0.1231])
     }
 
-    def test_gaussian_mixture_distribution_scalar_csi(self):
-        gm, _ = copula._gaussianMixtureDistribution(0.1, 0.52, self._params)
+    def test_solar_gmm_scalar_csi(self):
+        gm, _, _ = copula._solar_gmm(0.1, 0.52, self._params)
 
         assert gm.means_.shape == (2, 1)
 
-    def test_gaussian_mixture_distribution_vector_csi(self):
+    def test_solar_gmm_vector_csi(self):
         csi = np.linspace(-2, 2, 21)
-        gm, _ = copula._gaussianMixtureDistribution(csi, 0.52, self._params)
+        gm, _, _ = copula._solar_gmm(csi, 0.52, self._params)
 
         assert gm.means_.shape == (2, 1)
 
-    def test_gaussian_mixture_distribution_matlabvals(self):
-        gm, pdf_val = copula._gaussianMixtureDistribution(self._expect['in'], 0.52, self._params)
+    def test_solar_gmm_matlabvals(self):
+        gm, pdf_val, _ = copula._solar_gmm(self._expect['in'], 0.52, self._params)
         assert pdf_val == approx(self._expect['out'], abs=0.001)
 
 
@@ -69,14 +93,21 @@ class TestInverseSample:
         cdf = np.array([0.0, 0.25, 0.5, 0.75, 1.0])
         r = np.array([0.125, 0.5, 0.875])
 
-        out = copula.inverse_sample(x, cdf, r)
+        out = copula._inverse_sample(x, cdf, r)
+        assert out == approx(np.array([-1.5, 0.0, 1.5]))
+
+    def test_inverse_sample_complex_shape(self):
+        x = np.array([-2.0, -1.0, 0.0, 1.0, 2.0])
+        cdf = np.array([0.0, 0.1, 0.5, 0.9, 1.0])
+        r = np.array([0.05, 0.5, 0.95])
+        out = copula._inverse_sample(x, cdf, r)
         assert out == approx(np.array([-1.5, 0.0, 1.5]))
 
     def test_inverse_sample_uses_first_duplicate_index(self):
         x = np.array([-2.0, -1.0, 0.0, 2.0])
         cdf = np.array([0.0, 0.5, 0.5, 1.0])
 
-        out = copula.inverse_sample(x, cdf, 0.5)
+        out = copula._inverse_sample(x, cdf, 0.5)
         assert out == approx(-1.0)
 
     def test_inverse_sample_preserves_shape(self):
@@ -84,6 +115,5 @@ class TestInverseSample:
         cdf = np.array([0.0, 0.25, 0.5, 0.75, 1.0])
         r = np.array([[0.1, 0.2], [0.8, 0.9]])
 
-        out = copula.inverse_sample(x, cdf, r)
+        out = copula._inverse_sample(x, cdf, r)
         assert out.shape == r.shape
-
