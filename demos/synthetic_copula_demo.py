3d822cebb171dec9f870c46d995656ca44a84c8e
Joe Ranalli <jar339@psu.edu>
2026-06-22 17:22:05 -0400

    Generate plots to demonstrate the cloud field.

--- demos/synthetic_copula_demo.py
+++ demos/synthetic_copula_demo.py
@@ -79,6 +79,37 @@ def spatial_compare():
     plt.show()
 
 
+def distance_confirm():
+    import matplotlib.pyplot as plt
+
+    n = 60
+
+    cloud_spd = 10
+    dt = 10
+    cloud_dir = 0
+
+    mean_csi = 0.9
+
+    end_time = (pd.to_timedelta(f'{dt}s') * n).total_seconds()
+
+    times = pd.date_range(start='2024-01-01 00:00:00',
+                          end=f'2024-01-01 {int(end_time // 3600):02d}:{int((end_time - (end_time // 3600) * 3600) // 60):02d}:{int(end_time - (end_time // 60) * 60):02d}',
+                          freq=f'{dt}s')
+
+    e_pos = np.zeros(len(times))
+    n_pos = np.arange(len(times)) * cloud_spd * dt  # cld_spd * dt
+
+    c = downscale(times, e_pos, n_pos, cloud_spd, cloud_dir, mean_csi,
+                  DEFAULT_PARAMS, seed=42, scale=True, noneg=True)
+
+
+    plt.imshow(c, vmin=0.2, vmax=1.2)
+    plt.axis('equal')
+    plt.colorbar()
+    plt.show()
+
 if __name__ == '__main__':
-    matlab_compare()
-    spatial_compare()
+    distance_confirm()
+
+    # matlab_compare()
+    # spatial_compare()
